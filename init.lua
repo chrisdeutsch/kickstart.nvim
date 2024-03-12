755729ffb54b5e0e732c08846d732a25223a41bf
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2024-03-12 16:34:11 +0100

    Adding treesitter-context to nvim

--- init.lua
+++ init.lua
@@ -826,6 +826,13 @@ require('lazy').setup({
     end,
   },
 
+  { -- Show context of cursor position at top of buffer
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      multiline_threshold = 5,
+    },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
